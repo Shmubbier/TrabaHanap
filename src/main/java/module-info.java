4257d0907aa73a5b54982bc4263a2f3cf3acda0a
Charles <cnergy19@gmail.com>
2025-10-25 01:03:25 +0800

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.devera.trabahanap {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.devera.trabahanap to javafx.fxml;
+    exports com.devera.trabahanap;
+}
